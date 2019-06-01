95029b5800a452686b762ad85ee9a48acb6afed7
Cristian Torres <ctorresthrash@gmail.com>
2019-06-01 15:06:13 -0500

    fix form validation ReminderForm

--- src/components/ReminderForm.js
+++ src/components/ReminderForm.js
@@ -1,29 +1,51 @@
 import React from "react";
-import { Button } from "reacts";
+import { Button } from "reactstrap";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
+import TextInput from "./TextInput";
 
 const validationSchema = Yup.object().shape({
   content: Yup.string()
-    .length(30, "30 characters at most")
+    .max(30, "30 characters at most")
     .required("Required"),
   city: Yup.string().required("Required"),
   color: Yup.string().required("Required")
 });
 
-const ReminderForm = ({ onSubmit }) => {
+const ReminderForm = ({ onSubmit, initialValues }) => {
   return (
-    <Formik onSubmit={onSubmit} validationSchema={validationSchema}>
+    <Formik
+      enableReinitialize
+      onSubmit={onSubmit}
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+    >
       {() => {
         return (
           <Form>
             <Field
+              type="textarea"
               name="content"
               label="Content"
               placeholder="Enter your reminder"
+              component={TextInput}
             />
-            <Field name="city" label="City" placeholder="Enter your city" />
-            <Button color="primary">Submit</Button>
+            <Field
+              name="city"
+              label="City"
+              placeholder="Enter your city"
+              component={TextInput}
+            />
+            <Field
+              type="date"
+              name="date"
+              label="Date"
+              placeholder="Reminder's date"
+              component={TextInput}
+            />
+            <Button type="submit" color="primary">
+              Submit
+            </Button>
           </Form>
         );
       }}
