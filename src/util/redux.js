0cc91504e9ec81d5bfe06338bfe524d67d1f8a6b
Cristian Torres <ctorresthrash@gmail.com>
2019-06-02 16:41:11 -0500

    fix typo

--- src/util/redux.js
+++ src/util/redux.js
@@ -7,6 +7,6 @@ export const payloadIdentity = R.curry((name, payload) => ({
 export const loadingMutation = R.curry(({ name, type }, state, action) => ({
   ...state,
   [`loading${name}`]: type === "loading",
-  [`failure${name}`]: type === "failure" ? action.payload.failMessage : null,
+  [`failure${name}`]: type === "failure" ? action.payload.failureMessage : null,
   [`success${name}`]: type === "success"
 }));
